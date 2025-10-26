118cc11fe7d38437649c10dcd125b7b8ed2e2aaa
Gotman08 <nicolasmar@live.com>
2025-10-26 16:26:18 +0100

    cleanup golomb

--- CHPS0701ProgrammationParallèle/Golomb/scripts/format.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo "format code"
-find include src tests benchmarks -name "*.hpp" -o -name "*.cpp" | xargs clang-format -i
-echo "format ok"
--- CHPS0701ProgrammationParallèle/Golomb/scripts/run_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo "run tests"
-ctest --test-dir build --output-on-failure
-echo "tests ok"
